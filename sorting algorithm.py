ae528fde42aae36482973d3b7bff9687af9bcd6b
renati81 <renatisaisruthi@gmail.com>
2024-09-04 06:21:30 +0530

    Add files via upload

---
+++ sorting algorithm.py
@@ -0,0 +1,63 @@
+import time
+import random
+import matplotlib.pyplot as plt
+
+#insertion sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+#selection sort
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+#bubble sort
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+#benchmarking algorithm
+def benchmark_sorting_algorithm(sorting_func, input_sizes):
+    times = []
+    for size in input_sizes:
+        arr = random.sample(range(size * 10), size)
+        start_time = time.time()
+        sorting_func(arr.copy())
+        end_time = time.time()
+        times.append(end_time - start_time)
+    return times
+
+#input sizes
+input_sizes = [5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
+
+#benchmarking each algorithms
+insertion_times = benchmark_sorting_algorithm(insertion_sort, input_sizes)
+selection_times = benchmark_sorting_algorithm(selection_sort, input_sizes)
+bubble_times = benchmark_sorting_algorithm(bubble_sort, input_sizes)
+
+#plotting the sizes
+plt.plot(input_sizes, insertion_times, label="Insertion Sort", marker='o')
+plt.plot(input_sizes, selection_times, label="Selection Sort", marker='o')
+plt.plot(input_sizes, bubble_times, label="Bubble Sort", marker='o')
+plt.xlabel('Input Size')
+plt.ylabel('Time (seconds)')
+plt.title('Benchmark of Sorting Algorithms')
+plt.legend()
+plt.grid(True)
+plt.show()
